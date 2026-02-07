24bbfb84a64b66767721cac1bf44545599c56a37
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2026-02-07 14:48:35 +0545

    updated

--- securticket/securticket_backend/settings.py
+++ securticket/securticket_backend/settings.py
@@ -173,8 +173,12 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+
 ]
+
 CORS_ALLOW_CREDENTIALS = True
+
+
 
 # Security Settings
 SECURE_BROWSER_XSS_FILTER = True
